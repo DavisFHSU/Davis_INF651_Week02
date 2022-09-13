771a121177bad119b778b213c915d3c464d7ecb0
DavisFHSU <112444342+DavisFHSU@users.noreply.github.com>
2022-09-13 17:28:19 -0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,148 @@
+//Question 1
+
+let myNumber;
+
+console.log("myNumber = " + myNumber + " and is of type " + typeof myNumber);
+
+document.write("Question 1:");
+document.write("<br>");
+document.write("myNumber = " + myNumber + " and is of type " + typeof myNumber);
+document.write("<br>");
+document.write("<br>");
+
+//Question 2
+
+myNumber = 5;
+
+console.log("myNumber = " + myNumber + " and is of type " + typeof myNumber);
+
+document.write("Question 2:");
+document.write("<br>");
+document.write("myNumber = " + myNumber + " and is of type " + typeof myNumber);
+document.write("<br>");
+document.write("<br>");
+
+//Question 3
+
+let myOtherNumber;
+
+myOtherNumber = 5;
+
+console.log("myOtherNumber= " + myOtherNumber + " and is of type " + typeof myOtherNumber);
+
+document.write("Question 3:");
+document.write("<br>");
+document.write("myOtherNumber= " + myOtherNumber + " and is of type " + typeof myOtherNumber);
+document.write("<br>");
+document.write("<br>");
+
+
+//Question 4
+
+let myTotal;
+
+myTotal = myNumber * myOtherNumber;
+
+console.log("myTotal = " + myTotal + " and is of type " + typeof myTotal);
+
+document.write("Question 4:");
+document.write("<br>");
+document.write("myTotal = " + myTotal + " and is of type " + typeof myTotal);
+document.write("<br>");
+document.write("<br>");
+
+//Question 5
+
+let myName = "Brett";
+
+let myCombo = (myNumber + myOtherNumber);
+
+myCombo = myCombo.toString();
+
+console.log("myName = " + myName + " and is of type " + typeof myName);
+
+console.log("myCombo = " + myCombo + " and is of type " + typeof myCombo);
+
+document.write("Question 5:");
+document.write("<br>");
+document.write("myName = " + myName + " and is of type " + typeof myName);
+document.write("<br>");
+document.write("myCombo = " + myCombo + " and is of type " + typeof myCombo);
+document.write("<br>");
+document.write("<br>");
+
+
+
+//Question 6
+
+let myOtherCombo ="";
+
+myOtherCombo = (myNumber + myOtherNumber);
+
+console.log("myOtherCombo = " + myOtherCombo + " and is of type " + typeof myOtherCombo);
+
+document.write("Question 6:");
+document.write("<br>");
+document.write("myOtherCombo = " + myOtherCombo + " and is of type " + typeof myOtherCombo);
+document.write("<br>");
+document.write("<br>");
+
+
+//Question 7
+
+let myRemainder;
+
+myRemainder = myNumber%5; //needs to be 0
+
+console.log("myRemainder = " + myRemainder + " and is of type " + typeof myRemainder);
+
+document.write("Question 7:");
+document.write("<br>");
+document.write("myRemainder = " + myRemainder + " and is of type " + typeof myRemainder);
+document.write("<br>");
+document.write("<br>");
+
+
+//Question 8 
+
+let i;
+
+i = 2;
+
+console.log("i = " + i + " and is of type " + typeof i);
+
+document.write("Question 8:");
+document.write("<br>");
+document.write("i = " + i + " and is of type " + typeof i);
+document.write("<br>");
+document.write("<br>");
+
+//Question 9 
+
+let int;
+
+int = 4;
+
+console.log("int = " + int + " and is of type " + typeof int);
+
+document.write("Question 9:");
+document.write("<br>");
+document.write("int = " + int + " and is of type " + typeof int);
+document.write("<br>");
+document.write("<br>");
+
+
+//Question 10 
+
+let myBoolean = false; 
+
+console.log("myBoolean = " + myBoolean + " and is of type " + typeof myBoolean);
+
+document.write("Question 10:");
+document.write("<br>");
+document.write("myBoolean = " + myBoolean + " and is of type " + typeof myBoolean);
+document.write("<br>");
+document.write("<br>");
+
+
+
